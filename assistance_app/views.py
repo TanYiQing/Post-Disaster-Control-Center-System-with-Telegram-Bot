d03756bd8be93e723322baefd80f02cf5c8b22ab
TanYiQing <yiqingtan99@gmail.com>
2021-11-29 14:04:15 +0800

    update add.html

--- assistance_app/views.py
+++ assistance_app/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 def request_assistance(request):
     if request.method == 'POST':
         assistance_form = AssistanceForm(request.POST)
-        if  assistance_form.is_valid():
+        if assistance_form.is_valid():
             data = assistance_form.cleaned_data
             victim_number = data['victim_number']
             assistance_type = AssistanceType.objects.filter(name=data['assistance_type'])[0]
@@ -25,5 +25,14 @@ def request_assistance(request):
       
       
 def add(request):
+    if request.method == 'POST':
+        name = request.POST["name"]
+        ic_no = request.POST["ic_no"]
+        victim_num = request.POST["victim_num"]
+        assistance_type = request.POST["assistance_type"]
+        assistance_date = request.POST["assistance_date"]
+        created_at = request.POST["created_at"]
+        updated_at = request.POST["updated_at"]
+        remark = request.POST["remark"]
     return render(request, 'assistance_app/add.html')
 
