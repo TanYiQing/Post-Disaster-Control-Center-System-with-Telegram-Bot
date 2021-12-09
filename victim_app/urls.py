cd7165d31ad52c9a219164cb3b3470961d2072a1
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2021-12-09 16:56:04 +0700

    update profile

--- victim_app/urls.py
+++ victim_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('add_profile/', views.add_profile, name="add_profile"),
     path('list_user/', views.list_user, name="list_user"),
-    path('edit_profile/<ic>', views.edit_profile, name="edit_profile"),
+    path('edit_profile/', views.edit_profile, name="edit_profile"),
     path('delete_profile/', views.delete_profile, name="delete_profile"),
 ]
