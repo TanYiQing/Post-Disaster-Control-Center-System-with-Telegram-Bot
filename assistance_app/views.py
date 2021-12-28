460d3a4ed9c73527aa5238ecf066c7b334f60952
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2021-12-29 00:57:31 +0700

    Fix bug assistance app and add some html(#62)
    * change related_name in model and fix bug
    
    * update assistance_app

--- assistance_app/views.py
+++ assistance_app/views.py
@@ -9,14 +9,14 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login')
 def list_assistance_type(request):
-    assistances = request.user.assistance.all()
+    assistances = request.user.assistance_list.all()
     return render(request, 'assistance_app/list.html', {'assistances': assistances})
 
 
 @login_required(login_url='/login')
 def edit_assistance_type(request, id):
     try:
-        my_assistance = request.user.assistance.get(id=id)
+        my_assistance = request.user.assistance_list.get(id=id)
     except:
         return redirect('list_assistance_type')
 
@@ -25,13 +25,13 @@ def edit_assistance_type(request, id):
         assistance_type_id = int(request.POST["assistance_type"])
         remark = request.POST["remark"]
         assistance_type = AssistanceType.objects.get(id=assistance_type_id)
-        assistance = Assistance(victim_number=victim_num, remark=remark)
-        assistance.assistance_type = assistance_type
-        assistance.user = request.user
-        assistance.save()
+        my_assistance.assistance_type = assistance_type
+        my_assistance.remark = remark
+        my_assistance.victim_number = victim_num
+        my_assistance.save()
         return redirect('list_assistance_type')
     assistance_types = AssistanceType.objects.all()
-    return render(request, 'assistance_app/edit_user.html',
+    return render(request, 'assistance_app/edit_assistance.html',
                   {'my_assistance': my_assistance, 'assistance_types': assistance_types})
 
 
