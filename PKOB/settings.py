0e31270c91f371a380d76ef326cbb06058286180
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2021-12-29 14:24:25 +0700

    update settings.py

--- PKOB/settings.py
+++ PKOB/settings.py
@@ -96,9 +96,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env  = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# UNCOMMENT THIS CODE WHENEVER YOU WANT TO DEPLOY IT ON HEROKU
+# import dj_database_url
+# db_from_env  = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
