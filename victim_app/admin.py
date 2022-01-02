9f9ad56ffc9dda223ae8d3af9b07325aced5227b
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2022-01-02 07:16:47 +0700

    update backend without frontend

--- victim_app/admin.py
+++ victim_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from victim_app.models import Profile
+from victim_app.models import Victim
 
-admin.site.register(Profile)
+admin.site.register(Victim)
--- victim_app/views.py
+++ victim_app/views.py
@@ -2,19 +2,11 @@ import datetime
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render, redirect, get_object_or_404
-from victim_app.models import Profile
+from victim_app.models import Victim
 
 
 @login_required(login_url='/login')
 def add_profile(request):
-    user = request.user
-    try:
-        has_profile = user.profile is not None
-        if has_profile:
-            return redirect('edit_profile')
-    except:
-        print('user has no profile')
-        pass
 
     if request.method == "POST":
         phone = request.POST["phone"]
@@ -28,11 +20,10 @@ def add_profile(request):
         state = request.POST["state"]
         poskod = request.POST["poskod"]
         
-        victimProfile = Profile(phone=phone, is_kir=is_kir, salary=salary,
+        victim = Victim(phone=phone, is_kir=is_kir, salary=salary,
                                 address1=address1, address2=address2, city=city, mukim=mukim, parlimen=parlimen,
                                 state=state, poskod=poskod)
-        victimProfile.user = user
-        victimProfile.save()
+        victim.save()
         return redirect('edit_profile')
 
     return render(request, 'victim_app/add_profile.html')
@@ -40,23 +31,15 @@ def add_profile(request):
 
 @login_required(login_url='/login')
 def list_user(request):
-    victim_list = Profile.objects.all().order_by("-ic")
+    victim_list = Victim.objects.all().order_by("-ic")
     return render(request, 'victim_app/list_user.html',
                   context={'victim_list': victim_list})
 
 
 @login_required(login_url='/login')
-def edit_profile(request):
-    user = request.user
-    try:
-        has_profile = user.profile is not None
-        if not has_profile:
-            return redirect('add_profile')
-    except:
-        print('user has no profile')
-        pass
-    victim_profile = user.profile
-    if request.method == "POST" and (victim_profile != None):
+def edit_profile(request, id):
+    victim = get_object_or_404(Victim,id=id)
+    if request.method == "POST":
         phone = request.POST["phone"]
         is_kir = str(request.POST["is_kir"])
         salary = request.POST["salary"]
@@ -68,29 +51,29 @@ def edit_profile(request):
         state = request.POST["state"]
         poskod = request.POST["poskod"]
 
-        victim_profile.phone = phone
-        victim_profile.is_kir = is_kir
-        victim_profile.salary = salary
-        victim_profile.address1 = address1
-        victim_profile.address2 = address2
-        victim_profile.city = city
-        victim_profile.mukim = mukim
-        victim_profile.parlimen = parlimen
-        victim_profile.state = state
-        victim_profile.poskod = poskod
+        victim.phone = phone
+        victim.is_kir = is_kir
+        victim.salary = salary
+        victim.address1 = address1
+        victim.address2 = address2
+        victim.city = city
+        victim.mukim = mukim
+        victim.parlimen = parlimen
+        victim.state = state
+        victim.poskod = poskod
 
-        victim_profile.save()
+        victim.save()
         respond = "Edit Successful"
-        return render(request, 'victim_app/edit_profile.html', context={'victim_profile': victim_profile})
+        return render(request, 'victim_app/edit_profile.html', context={'victim': victim})
 
-    return render(request, 'victim_app/edit_profile.html', context={'victim_profile': victim_profile})
+    return render(request, 'victim_app/edit_profile.html', context={'victim': victim})
 
 
-def delete_profile(request):
+def delete_profile(request,id):
     context = {}
     if "ic" in request.GET:
-        ic = request.GET["ic"]
-        icd = get_object_or_404(Profile, ic=ic)
+        id = request.GET["ic"]
+        icd = get_object_or_404(Victim, id=id)
         context["victim"] = icd
 
         if "action" in request.GET:
