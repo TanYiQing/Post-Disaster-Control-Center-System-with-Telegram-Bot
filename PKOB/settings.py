83454997a5ea454b53a291b7bfacd7de163c1c77
TanYiQing <yiqingtan99@gmail.com>
2022-01-04 17:32:20 +0800

    update settings.py

--- PKOB/settings.py
+++ PKOB/settings.py
@@ -96,7 +96,6 @@ DATABASES = {
 
 # UNCOMMENT THIS CODE WHENEVER YOU WANT TO DEPLOY IT ON HEROKU
 import dj_database_url
-
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
