73a118c2f4b90b2957ac0c9f67f7373aaf5ca0b9
TanYiQing <yiqingtan99@gmail.com>
2022-01-05 12:06:03 +0800

    telegram bot deployment

--- PKOB/settings.py
+++ PKOB/settings.py
@@ -95,9 +95,9 @@ DATABASES = {
 }
 
 # UNCOMMENT THIS CODE WHENEVER YOU WANT TO DEPLOY IT ON HEROKU
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
