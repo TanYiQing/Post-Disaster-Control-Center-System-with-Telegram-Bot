36a5b80440a6e117d2309ae5aacc83c3f7805840
NG SHEN MENG <shenmeng0124@gmail.com>
2022-01-08 23:10:15 +0800

    update

--- victim_app/views.py
+++ victim_app/views.py
@@ -1,11 +1,8 @@
-import datetime
 from django.contrib.auth.decorators import login_required
-
 from django.shortcuts import render, redirect, get_object_or_404
 from victim_app.models import Victim
 
 
-@login_required(login_url='/login')
 def add_profile(request):
 
     if request.method == "POST":
@@ -26,21 +23,20 @@ def add_profile(request):
                                 address1=address1, address2=address2, city=city, mukim=mukim, parlimen=parlimen,
                                 state=state, poskod=poskod)
         victim.save()
-        return redirect('list_victim')
+        return redirect('list_user')
 
     return render(request, 'victim_app/add_profile.html')
 
 
-# @login_required(login_url='/login')
-# def list_user(request):
-#     victim_list = Victim.objects.all().order_by("-ic")
-#     return render(request, 'victim_app/list_user.html',
-#                   context={'victim_list': victim_list})
+def list_user(request):
+    # victim_list = Victim.objects.all().order_by("-ic")
+    return render(request, 'victim_app/list_user.html',
+                  # context={'victim_list': victim_list}
+                  )
 
 
-@login_required(login_url='/login')
-def edit_profile(request, id):
-    victim = get_object_or_404(Victim,id=id)
+def edit_profile(request):
+    # victim = get_object_or_404(Victim,id=id)
     if request.method == "POST":
         name = request.POST['name']
         ic = request.POST['icNum']
@@ -55,33 +51,37 @@ def edit_profile(request, id):
         state = request.POST["state"]
         poskod = request.POST["poskod"]
 
-        victim.name = name
-        victim.ic = ic
-        victim.phone = phone
-        victim.is_kir = is_kir
-        victim.salary = salary
-        victim.address1 = address1
-        victim.address2 = address2
-        victim.city = city
-        victim.mukim = mukim
-        victim.parlimen = parlimen
-        victim.state = state
-        victim.poskod = poskod
-
-        victim.save()
+        # victim.name = name
+        # victim.ic = ic
+        # victim.phone = phone
+        # victim.is_kir = is_kir
+        # victim.salary = salary
+        # victim.address1 = address1
+        # victim.address2 = address2
+        # victim.city = city
+        # victim.mukim = mukim
+        # victim.parlimen = parlimen
+        # victim.state = state
+        # victim.poskod = poskod
+        #
+        # victim.save()
         respond = "Edit Successful"
         return redirect('list_victim')
     
-    return render(request, 'victim_app/edit_profile.html', context={'victim': victim})
+    return render(request, 'victim_app/edit_profile.html')
 
 
-def delete_profile(request,id):
-    context = {}
-    icd = get_object_or_404(Victim, id=id)
-    context["victim"] = icd
+def victim_dashboard(request):
+    return render(request, 'victim_app/victim_dashboard.html')
 
-    if "action" in request.GET:
-        icd.delete()
-        context["status"] = str(icd.name) + " Removed Successfully"
-        return redirect('list_victim')
-    return render(request, 'victim_app/delete_profile.html', context)
+
+def request_assistance(request):
+    return render(request, 'victim_app/victim_request_assistance.html')
+
+
+def edit_assistance(request):
+    return render(request, 'victim_app/victim_edit_assistance.html')
+
+
+def view_assistance(request):
+    return render(request, 'victim_app/list_victim_assistance.html')
